f186078669e9772bfefb5d445e1e5c1c0e1c57f9
lol-cubes <arinmkhare@gmail.com>
2023-07-20 15:09:49 -0400

    add RDF python script

---
+++ scripts/rdf.py
@@ -0,0 +1,95 @@
+"""
+Computes the O-O radial distribution function of water
+written by Arin Khare
+
+usage: python3 rdf.py <traj.xtc> <top.gro> <output_path>
+
+- Outputs a CSV file containing these columns:
+    - The right edge of each bin, in nm
+    - The frequency of distances for each bin (averaged over all frames)
+    - The total volume for each bin
+    - The number density corresponding to each bin
+    - The normalized RDF value for each bin
+- As a test, this should return the same results for trajectories before and after running gmx trjconv
+"""
+
+import itertools
+import math
+import sys
+import time
+
+from fast_histogram import histogram1d
+import mdtraj as md
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+NUM_BINS = 1000
+
+
+def get_rdf(traj):
+    print("Calculating volumes... ", end="", flush=True)
+    oxygens = traj.topology.select("symbol == O")
+    max_dist = math.sqrt(3) * (np.max(traj.unitcell_volumes) ** (1 / 3) / 2)
+    dr = max_dist / NUM_BINS
+    # The value to divide by is the number of oxygens times the volume of the spherical shell,
+    # since we are averaging over all oxygens. This will ensure we are actually calculating
+    # number density, not something proportional to it.
+    total_volumes = np.zeros(NUM_BINS)
+    prev_sphere_vol = 0
+    for i in range(NUM_BINS):
+        sphere_vol = (4 / 3) * math.pi * (dr * (i + 1)) ** 3
+        volume = sphere_vol - prev_sphere_vol
+        prev_sphere_vol = sphere_vol
+        total_volumes[i] = volume * len(oxygens)
+    print("done")
+
+    print("Calculating distances... ", end="", flush=True)
+    o_pairs = np.array(list(itertools.combinations(oxygens, 2)))
+    distances = md.compute_distances(traj, o_pairs)  # Follows minimum image convention
+    plt.hist(distances[1000], bins=500)
+    plt.show()
+    distances = np.ravel(distances)
+    print("done")
+
+    print("Calculating frequencies... ", end="", flush=True)
+    frequencies = histogram1d(distances, NUM_BINS, range=(0, max_dist + dr / 2))
+    print("done")
+
+    print("Normalizing distribution... ", end="", flush=True)
+    avg_num_density = len(oxygens) / np.mean(traj.unitcell_volumes)
+    # Number of frames for each radius that have unitcells large enough to contain that radius.
+    num_frames = np.zeros(NUM_BINS)
+    for x, y, z in traj.unitcell_lengths:
+        diagonal = 0.5 * math.sqrt((x) ** 2 + (y) ** 2 + (z) ** 2)
+        for i in range(NUM_BINS):
+            r = dr * i
+            if diagonal >= r:
+                num_frames[i] += 1
+            else:
+                break
+    # Multiply by 2 first because each distance needs to be counted twice.
+    frequency_rates = (2 * frequencies) / num_frames
+    rdf = frequency_rates / total_volumes
+    normalized_rdf = rdf / avg_num_density
+    print("done")
+
+    return np.transpose(
+        [np.arange(dr, max_dist + dr / 2, dr),
+         frequencies,
+         total_volumes,
+         rdf,
+         normalized_rdf]) 
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    outfile = sys.argv[3]
+    print("Reading trajectory file... ", end="", flush=True)
+    traj = md.load(sys.argv[1], top=sys.argv[2])
+    print("done")
+    rdf = get_rdf(traj)
+    print("Writing to file... ", end="", flush=True)
+    np.savetxt(outfile, rdf, delimiter="  ")
+    print(f"done. RDF data saved to {outfile}")
+    print(f"Program finished in {time.time() - start_time} seconds")
